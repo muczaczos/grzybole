a1d379cdac28f24279bd483a543c4e5d42124327
Arek Szatkowski <muczaczos@gmail.com>
2022-10-12 16:00:28 +0200

    Front page - Preparing theme for carousel

--- functions.php
+++ functions.php
@@ -142,11 +142,16 @@ function grzybole_scripts() {
 	wp_enqueue_style( 'grzybole-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_enqueue_style( 'grzybole-main', get_template_directory_uri() . '/css/main.css');
 	wp_enqueue_style( 'bootstrap-icons', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css');
-
-
 	wp_style_add_data( 'grzybole-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'grzybole-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'bootstrap-popper', "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js", array('jquery'));
+	wp_enqueue_script( 'bootstrap-script', "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js", array('jquery'));
+	wp_enqueue_script( 'grzybole-script', get_template_directory_uri() . '/js/script.js', array('jquery'));
+
+	
+	
+	
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
