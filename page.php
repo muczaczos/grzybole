2dbdaa5bc4ca0849212d123df92e1215838ba6af
Arek Szatkowski <muczaczos@gmail.com>
2022-10-19 11:47:35 +0200

    Page - improved page look

--- page.php
+++ page.php
@@ -14,25 +14,25 @@
 
 get_header();
 ?>
+ 
+		<main id="primary" class="site-main">
+			<div class="container pt-5">
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-	<main id="primary" class="site-main">
+				get_template_part( 'template-parts/content', 'page' );
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+			endwhile; // End of the loop.
+			?>
+			</div>
+		</main><!-- #main -->
+	
 
 <?php
-get_sidebar();
 get_footer();
