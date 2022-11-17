104000c0cea09fe4d97499e10fa1312dd8b53b9e
Arek Szatkowski <muczaczos@gmail.com>
2022-11-17 15:23:24 +0100

    Added custom 404 page

--- 404.php
+++ 404.php
@@ -14,44 +14,15 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'grzybole' ); ?></h1>
+				<h1 class="page-title pt-5 text-center text-primary"><?php esc_html_e( 'Uups! Chyba zgubiłeś się w naszym grzybowym lesie :)', 'grzybole' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'grzybole' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'grzybole' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$grzybole_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'grzybole' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$grzybole_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+				<p class="text-center"><?php esc_html_e( 'To nic takiego. Po prostu użyj wyszukiwarki na górze srony lub naciśnij na grzyb, żeby wrócić do grzybolowego domu :)', 'grzybole' ); ?></p>
 			</div><!-- .page-content -->
+			<div class="text-center pb-5">
+				<img class="center" src="<?php echo get_template_directory_uri();?>/img/slider/404.svg" alt="..." loading="lazy">
+		  </div>
 		</section><!-- .error-404 -->
 
 	</main><!-- #main -->
