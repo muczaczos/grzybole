3191ed2f15802ea98fa0fdf2b6858372fa6d92ab
Arek Szatkowski <arekszatkowski@mbp-arek.home>
2022-11-22 17:50:22 +0100

    added shortcode with products to laboratorium category

--- LabPage.php
+++ LabPage.php
@@ -21,13 +21,20 @@ get_header();
 	
 		<section class="categories pt-5 pb-5">
 			<div class="container">
-			  <h1 class="text-center text-primary">Laboratorium</h1>
-        <div class="row pt-5 align-items-center">
-            <div class="col-md-6 col-sm-12 pb-5"><img class="" src="<?php echo get_template_directory_uri();?>/img/slider/lab.svg" alt="Sprzęt do mykologii" loading="lazy"></div>  
-            <div class="col-md-6 col-sm-12"><p class="text-primary align-middle">Laboratorium to moja kuchnia, w której przygotowuje jedzenie dla grzybów. Ta kategoria zawiera produkty, niezbędne w każdym mykologicznym laboratorium.</p></div>
-			</div>
-        </div>
+			    <h1 class="text-center text-primary">Laboratorium</h1>
+                <div class="row pt-5 align-items-center">
+                    <div class="col-md-6 col-sm-12 pb-5"><img class="" src="<?php echo get_template_directory_uri();?>/img/slider/lab.svg" alt="Sprzęt do mykologii" loading="lazy"></div>  
+                    <div class="col-md-6 col-sm-12"><p class="text-primary align-middle">Laboratorium to moja kuchnia, w której przygotowuje jedzenie dla grzybów. Ta kategoria zawiera produkty, niezbędne w każdym mykologicznym laboratorium.</p></div>
+			    </div>
+            </div>
 		</section>
+        <section class="categories pt-5 pb-5 bg-white">
+		    <div class="container">
+                <div class="pb-5">
+				    <?php echo do_shortcode('[products columns=4 category=laboratorium]'); ?>
+                </div>
+	         </div>
+        </section>
 	</main>
 
 <?php
